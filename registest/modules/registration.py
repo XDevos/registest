5b0909a4b0ba3ddb5cbf70a726bc8a26e53dfb7d
Xavier DEVOS <xavierdevos@outlook.fr>
2025-01-26 10:50:02 +0100

    Dev(HL) - Add 'regis_registration' to script names callable

--- registest/modules/registration.py
+++ registest/modules/registration.py
@@ -2,7 +2,18 @@
 
 from skimage.registration import phase_cross_correlation
 
+from registest.utils.metrics import timing_main
+
 
 def phase_cross_correlation_wrapper(ref_3d, target_3d):
     shift, _, _ = phase_cross_correlation(ref_3d, target_3d, upsample_factor=100)
     return shift
+
+
+@timing_main
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
